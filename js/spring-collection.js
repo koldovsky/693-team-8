2060b8fb828374a0030e8ed96916f95b9bcfc3c8
Sabina <46112061+pumba299@users.noreply.github.com>
2022-05-27 14:36:35 +0200

    added style for js within springcollection block

--- js/spring-collection.js
+++ js/spring-collection.js
@@ -2,8 +2,6 @@
     const clock = document.querySelector('.clock');
     function updateClock() {
         clock.innerHTML = new Date().toLocaleTimeString();
-        // clock.style.color = 'red';
-        // clock.classList.add('select');
     }
     setInterval(updateClock, 1000);
 })();
